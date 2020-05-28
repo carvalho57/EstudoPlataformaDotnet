0d643f9f2f69eb8813fa58018c82e12ca256ecf5
carvalho57 <gabrielfeocarvalho57@gmail.com>
2020-05-28 19:56:33 -0300

    Iniciando o tutorial MVC Movie

---
+++ tutorial/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace MvcMovie.Controllers {
+    public class HelloWorldController : Controller {
+        public string Index() {
+            return "This is my default action....";
+        }
+
+        public string Welcome() {
+            return "This is my Welcome action method...";
+        }
+    }
+}
