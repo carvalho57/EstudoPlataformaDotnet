20d21e4bad0de12fe81820be552df289b64c7b7b
carvalho57 <gabrielfeocarvalho57@gmail.com>
2020-06-02 13:40:17 -0300

    Add a View

--- tutorial/MvcMovie/Controllers/HelloWorldController.cs
+++ tutorial/MvcMovie/Controllers/HelloWorldController.cs
@@ -1,15 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-
+using System.Text.Encodings.Web;
 
 
 namespace MvcMovie.Controllers {
     public class HelloWorldController : Controller {
-        public string Index() {
-            return "This is my default action....";
+
+        //Get
+        public IActionResult Index() {
+            return View();
+        }
+        //Get
+        public string Welcome(string name, int numTimes = 1) {
+            ViewData["Message"] = $"Hello {name}";
+            ViewData["NumTimes"] = {ID};
+
+
         }
 
-        public string Welcome() {
-            return "This is my Welcome action method...";
-        }
+            
     }
 }
